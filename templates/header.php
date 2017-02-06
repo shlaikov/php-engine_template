16899679f7835d8b0f5d29f6be3f13f671cf2feb
shlaikov <shlaikov.alexey@ya.ru>
2017-02-06 04:41:47 +0300

    Add some code.

--- templates/header.php
+++ templates/header.php
@@ -5,11 +5,13 @@
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="msapplication-tap-highlight" content="no">
 		<meta name="description" content="">
 		<meta name="author" content="">
+		<meta name="theme-color" content="#fff">
 		<link rel="icon" href="favicon.ico">
 		<title><?php echo $engine->getTitle(); ?></title>
-		<!-- Bootstrap -->
+		<link rel="manifest" href="manifest.json">
 		<link href="styles/bootstrap.min.css" rel="stylesheet">
 		<link href="styles/style.css" rel="stylesheet">
 		<!--[if lt IE 9]>
