f3795d4c4c70b1336b09634fa42802f2c96219e5
Alexey Shlaikov <alexikz31@gmail.com>
2017-02-06 04:17:20 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once "class/Engine.php";
+$engine = new Engine();
+ 
+include_once "templates/header.php";
+
+if ($engine->getError()) {
+    echo "<div style='border:1px solid red;padding:10px;margin: 10px auto; 
+        width: 500px;'>" .$engine->getError(). "</div>";
+}
+echo $engine->getContentPage();
+
+include_once "templates/footer.php";
+
+?>
